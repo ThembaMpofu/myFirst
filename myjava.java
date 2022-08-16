5d452bb23f5951cd3567a3e9cc1a57337f2ed91a
ThembaMpofu <111359277+ThembaMpofu@users.noreply.github.com>
2022-08-16 16:53:10 +0200

    changed 3
    Signed-off-by: ThembaMpofu <111359277+ThembaMpofu@users.noreply.github.com>

--- myjava.java
+++ myjava.java
@@ -2,6 +2,6 @@ public class myjava {
 
      public static void main(String[] args) {
           System.out.println(" hello github");
-          System.out.println("ku bird");
+          System.out.println("ku birdjjgjgjgj");
      }
 }
