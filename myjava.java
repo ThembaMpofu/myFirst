69df0504ebef882656a1b48431d38f20d60e28be
ThembaMpofu <111359277+ThembaMpofu@users.noreply.github.com>
2022-08-16 17:44:30 +0200

    init
    Signed-off-by: ThembaMpofu <111359277+ThembaMpofu@users.noreply.github.com>

--- myjava.java
+++ myjava.java
@@ -1,7 +1,7 @@
 public class myjava {
 
      public static void main(String[] args) {
-          System.out.println(" hello github");
+          System.out.println(" hello githubbhhh");
           System.out.println("ku birdjjgjgjgj");
      }
 }
